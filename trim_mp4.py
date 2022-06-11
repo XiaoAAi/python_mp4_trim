1ad419c2e731e2a8416bfe9ee5d3ac9769db7a40
yuBW.com <YBW>
2022-06-11 23:31:28 +0800

    修改处理流程:1.剪切视频；2.压缩剪切的视频;3.压缩视频转成ts;4.ts和end.ts拼接成mp4最终视频

--- trim_mp4.py
+++ trim_mp4.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+
 import datetime
 import os
 import ffmpeg
@@ -35,7 +36,8 @@ def get_files_and_mkdir_mutil(video_path, new_path):
         list_new_file_path.append(filepath.replace(video_path, new_path, 1))
     # 将最新文件名和视频文件名放到字典 map_old_new_path 做对应
     for filename in list_file_path_name:
-        map_old_new_path[filename] = filepath.replace(video_path, new_path, 1)
+        map_old_new_path[filename] = filename.replace(video_path, new_path, 1)
+        # print(filename, filename.replace(video_path, new_path, 1))
     # 复制多文件夹到新的路径下
     if os.path.isfile(str_file_mkdir_state):
         os.remove(str_file_mkdir_state)
@@ -83,10 +85,10 @@ if __name__ == '__main__':
     # str_time_begin = get_ffmpeg_format_time(time_begin)   # 开始时间格式处理
     # print(str_time_begin)
 
-
     if os.path.isfile(str_file_result):
         os.remove(str_file_result)
 
+    # 先剪切视频，将视频直接压缩2500Kbps码率  之后转成ts格式，直接进行拼接
     for video_name in map_old_new_path.keys():
         timeVideo = float(ffmpeg.probe(video_name)['format']['duration'])
         # print(timeVideo)
@@ -101,11 +103,15 @@ if __name__ == '__main__':
         # print(str_cmd_crop)
         os.system(str_cmd_crop)
 
-        str_cmd_splicing = "ffmpeg.exe -i .\\TMP.mp4 -i .\\ADD\\end.mp4 -filter_complex \"[0:0] [0:1] [1:0] [1:1] concat=n=2:v=1:a=1 [v] [a]\" -map \"[v]\" -map \"[a]\" -b 2500K " + map_old_new_path[video_name] + " -y"
-        # print(str_cmd_splicing)
-        os.system(str_cmd_splicing)
+        str_cmd_compress = "ffmpeg.exe -i .\\TMP.mp4 -b 2500K .\\compress_output.mp4 -y"
+        os.system(str_cmd_compress)
 
-        # do_system_cmd(str_cmd_crop,str_cmd_splicing)
+        str_cmd_mp42ts = "ffmpeg.exe -i .\\compress_output.mp4 -c:v copy .\\compress_output.ts -y"
+        os.system(str_cmd_mp42ts)
+
+        str_cmd_concat = "ffmpeg.exe -i \"concat:compress_output.ts|.\\ADD\\end.ts\" -c copy " + map_old_new_path[video_name] + " -y"
+        print(str_cmd_concat)
+        os.system(str_cmd_concat)
 
         result = open(str_file_result, 'a+')
         new_video_size = int(int(ffmpeg.probe(map_old_new_path[video_name])['format']['size']) / 1024 / 1024)
@@ -113,8 +119,63 @@ if __name__ == '__main__':
             result.writelines(map_old_new_path[video_name] + "\t SIZE:OverFlower 500M" + '\t Completed\n')
         else:
             result.writelines(map_old_new_path[video_name] + "\t SIZE:" + str(new_video_size) + 'M' + '\t Completed\n')
-
         result.close()
+
+
+
+
+
+
+
+
+
+
+
+
+    # 问题：某些视频会出现 More than 1000 frames duplicated 导致剪切视频特别长
+    # for video_name in map_old_new_path.keys():
+    #     timeVideo = float(ffmpeg.probe(video_name)['format']['duration'])
+    #     # print(timeVideo)
+    #     timeVideo = timeVideo - time_end
+    #     # print(timeVideo)
+    #     str_time_video = get_ffmpeg_format_time(timeVideo)
+    #     # print(str_time_video)
+    #     if os.path.isfile("./TMP.mp4"):
+    #         os.remove('./TMP.mp4')
+    #     str_cmd_crop = "ffmpeg.exe -ss 00:00:00.00  -i " + video_name +\
+    #         " -vcodec copy -acodec copy -t " + str_time_video +" TMP.mp4 -y"
+    #     # print(str_cmd_crop)
+    #     os.system(str_cmd_crop)
+    #
+    #     if int(int(ffmpeg.probe(video_name)['format']['bit_rate']) / 1000) >= 10000:
+    #         str_cmd_compress = "ffmpeg.exe -i .\\TMP.mp4 -b 2500K .\\compree_output.mp4 -y"
+    #         os.system(str_cmd_compress)
+    #
+    #         str_cmd_splicing = "ffmpeg.exe -i .\\compree_output.mp4 -i .\\ADD\\end.mp4 -filter_complex \"[0:0] [0:1] [1:0] [1:1] concat=n=2:v=1:a=1 [v] [a]\" -map \"[v]\" -map \"[a]\" -b 2500K " + map_old_new_path[video_name] + " -y"
+    #         print(str_cmd_splicing)
+    #         os.system(str_cmd_splicing)
+    #
+    #     else:
+    #         str_cmd_splicing = "ffmpeg.exe -i .\\TMP.mp4 -i .\\ADD\\end.mp4 -filter_complex \"[0:0] [0:1] [1:0] [1:1] concat=n=2:v=1:a=1 [v] [a]\" -map \"[v]\" -map \"[a]\" -b 2500K " + map_old_new_path[video_name] + " -y"
+    #         # print(str_cmd_splicing)
+    #         os.system(str_cmd_splicing)
+    #
+    #     # do_system_cmd(str_cmd_crop,str_cmd_splicing)
+    #
+    #     result = open(str_file_result, 'a+')
+    #     new_video_size = int(int(ffmpeg.probe(map_old_new_path[video_name])['format']['size']) / 1024 / 1024)
+    #     if new_video_size > 500 :
+    #         result.writelines(map_old_new_path[video_name] + "\t SIZE:OverFlower 500M" + '\t Completed\n')
+    #     else:
+    #         result.writelines(map_old_new_path[video_name] + "\t SIZE:" + str(new_video_size) + 'M' + '\t Completed\n')
+    #
+    #     result.close()
+
+
+
+
+
+
 
 
 
